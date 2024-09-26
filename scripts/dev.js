26bacf2599c99be3ef50faa454d4131eead61f5a
devcui <devcui@outlook.com>
2024-09-26 15:05:08 +0800

    feat: add project structor

---
+++ scripts/dev.js
@@ -0,0 +1,13 @@
+const os = require("os");
+const { spawn } = require("child_process");
+const path = require("path");
+const isWindows = os.platform() === "win32";
+const root = path.resolve(__dirname, "../");
+const bin = path.resolve(__dirname, "../node_modules/.bin");
+const tsc = path.resolve(bin, isWindows ? "tsc.cmd" : "tsc");
+const sass = path.resolve(bin, isWindows ? "sass.cmd" : "sass");
+const tsconfig = path.resolve(root, "tsconfig.json");
+const process_tsc = spawn(tsc, ["--watch", "--project", tsconfig], { stdio: "inherit", });
+const process_sass = spawn(sass, ["--watch", "src:dist"], { stdio: "inherit" });
+process_tsc.on("data", (data) => { console.log(data); });
+process_sass.on("data", (data) => { console.log(data); });
